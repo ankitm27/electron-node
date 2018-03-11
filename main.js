efa6571e62e6536efaa1650a79bc6b4d082838a1
ankitm27 <ankit.mahotra00@gmail.com>
2018-03-11 18:11:49 +0530

    changes

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const {app, BrowserWindow} = require('electron')
 const url = require('url')
 const path = require('path')
-const {ipcMain} = require('electron')
 
 let win
 
@@ -13,21 +12,5 @@ function createWindow() {
         slashes: true
     }))
 }
-
-// Event handler for asynchronous incoming messages
-ipcMain.on('asynchronous-message', (event, arg) => {
-    console.log(arg)
-
-    // Event emitter for sending asynchronous messages
-    event.sender.send('asynchronous-reply', 'async pong')
-})
-
-// Event handler for synchronous incoming messages
-ipcMain.on('synchronous-message', (event, arg) => {
-    console.log(arg)
-
-    // Synchronous event emmision
-    event.returnValue = 'sync pong'
-})
 
 app.on('ready', createWindow)
